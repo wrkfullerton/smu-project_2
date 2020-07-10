f3b1ed77dcc2cb735fd017792ee073a8e4638862
William Fullerton <wrk.fullerton@gmail.com>
2020-07-09 20:59:26 -0500

    deleted db folder-unneeded

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO shoes (Brand, Name, Size, Color, Condition_shoe) VALUES ('Nike', 'Nike Air Zoom Pegasus 37', '11', 'White', 'New');
-INSERT INTO shoes (Brand, Name, Size, Color, Condition_shoe) VALUES ('Brooks', 'Levitate 2', '11', 'Orange', 'Used');
-INSERT INTO shoes (Brand, Name, Size, Color, Condition_shoe) VALUES ('Saucony', 'Triumph 17', '11', 'Blue', 'Used');
