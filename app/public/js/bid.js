7def553f0cb57e02ee78a2554977b42c5438484a
Isaias Quintanilla <quintanillaisaias@gmail.com>
2020-07-14 20:13:16 -0500

    user can post bid amount for shoe

--- app/public/js/bid.js
+++ app/public/js/bid.js
@@ -5,7 +5,7 @@ let timeRemaining = 60;
 
 submitButton.addEventListener("click", timeKeeper);
 
-// function for Timer
+// Function for Timer
 function timeKeeper() {
     interval = setInterval(function () {
         timeRemaining--;
@@ -17,3 +17,25 @@ function timeKeeper() {
 
     }, 1000)
 };
+
+// Text for Shoe Details
+// $.get("/api", function (data) {
+//     for (var i = 0; i < data.length; i++) {
+
+//         var wellSection = $("<div>");
+//         wellSection.addClass("well");
+//         wellSection.attr("id", "shoe-wll-" + i);
+//         $("well-section").append(wellSection);
+
+//         $("#shoe-well-" + i).append("<h2>Brand: " + data[i].brand + "</h2>");
+//         $("#shoe-well-" + i).append("<h3>Name: " + data[i].name + "</h3>");
+//         $("#shoe-well-" + i).append("<h4>Size: " + data[i].size + "</h4>");
+//         $("#shoe-well-" + i).append("<h4>Color: " + data[i].color + "</h4>");
+
+//     };
+// });
+
+// when user submits their bid amount
+$("#submit-btn").click(function () {
+    $('.current-bid').append($(".form-control").val());
+});
