38ebd0a0e0d984f9a9c51a4cb7e85787bcb93ea8
Isaias Quintanilla <quintanillaisaias@gmail.com>
2020-07-15 21:16:57 -0500

    Moved shoe details in bid page and removed timer

--- app/public/js/bid.js
+++ app/public/js/bid.js
@@ -5,35 +5,18 @@ let timeRemaining = 60;
 
 submitButton.addEventListener("click", timeKeeper);
 
-// Function for Timer
+// Function for Timer (For Future use)
 function timeKeeper() {
-    interval = setInterval(function () {
-        timeRemaining--;
-        timer.innerHTML = "Time remaining : " + timeRemaining;
-        if (timeRemaining <= 0 || index >= 5) {
-            clearInterval(interval);
-            bidOver();
-        }
+    // interval = setInterval(function () {
+    //     timeRemaining--;
+    //     timer.innerHTML = "Time remaining : " + timeRemaining;
+    //     if (timeRemaining <= 0 || index >= 5) {
+    //         clearInterval(interval);
+    //         bidOver();
+    //     }
 
-    }, 1000)
+    // }, 1000)
 };
-
-// Text for Shoe Details
-// $.get("/api", function (data) {
-//     for (var i = 0; i < data.length; i++) {
-
-//         var wellSection = $("<div>");
-//         wellSection.addClass("well");
-//         wellSection.attr("id", "shoe-wll-" + i);
-//         $("well-section").append(wellSection);
-
-//         $("#shoe-well-" + i).append("<h2>Brand: " + data[i].brand + "</h2>");
-//         $("#shoe-well-" + i).append("<h3>Name: " + data[i].name + "</h3>");
-//         $("#shoe-well-" + i).append("<h4>Size: " + data[i].size + "</h4>");
-//         $("#shoe-well-" + i).append("<h4>Color: " + data[i].color + "</h4>");
-
-//     };
-// });
 
 // when user submits their bid amount
 $("#submit-btn").click(function () {
